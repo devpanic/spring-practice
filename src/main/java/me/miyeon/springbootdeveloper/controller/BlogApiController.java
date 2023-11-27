8080256820763782cdbd4c0d4b3f23e4a227fe41
devpanic <yeon.dev.8@gmail.com>
2023-11-27 17:06:26 +0900

    feat : 글 목록 조회

--- src/main/java/me/miyeon/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/miyeon/springbootdeveloper/controller/BlogApiController.java
@@ -1,11 +1,14 @@
 package me.miyeon.springbootdeveloper.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.miyeon.springbootdeveloper.domain.Article;
 import me.miyeon.springbootdeveloper.dto.AddArticleRequest;
+import me.miyeon.springbootdeveloper.dto.ArticleResponse;
 import me.miyeon.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,20 @@ public class BlogApiController {
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/me/miyeon/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/miyeon/springbootdeveloper/service/BlogService.java
@@ -1,5 +1,6 @@
 package me.miyeon.springbootdeveloper.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.miyeon.springbootdeveloper.domain.Article;
 import me.miyeon.springbootdeveloper.dto.AddArticleRequest;
@@ -11,7 +12,11 @@ import org.springframework.stereotype.Service;
 public class BlogService {
     private final BlogRepository blogRepository;
 
-    public Article save(AddArticleRequest request){
+    public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
