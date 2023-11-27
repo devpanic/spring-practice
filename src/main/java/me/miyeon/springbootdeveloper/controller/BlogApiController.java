a8cb85d1c4cca83a46d0ff50c0b7c30b4b0f2fa3
devpanic <yeon.dev.8@gmail.com>
2023-11-27 18:01:07 +0900

    feat : 블로그 글 단일 조회

--- src/main/java/me/miyeon/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/miyeon/springbootdeveloper/controller/BlogApiController.java
@@ -9,6 +9,7 @@ import me.miyeon.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,13 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> finArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
